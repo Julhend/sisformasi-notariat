7cf98f1704f0fa208160b3260a321505ac592a48
Jul Hendri <meetredmist@gmail.com>
2022-02-10 15:01:58 +0700

    oke nh bos

--- resources/views/peralihanwaris/index.blade.php
+++ resources/views/peralihanwaris/index.blade.php
@@ -92,7 +92,26 @@
                                 <a href="/peralihanwaris/{{$data->id}}/dokumen"
                                     class="btn btn-primary btn-sm my-1 mr-sm-1 btn-block"><i
                                     class="nav-icon fas fa-upload"></i> dokumen</a>
-                                <a href="/peralihanwaris/{{$data->id}}/upload-akta"
+
+                                    @if($data->status == 'pending')
+                                    <a href="/peralihanwaris/{{$data->id}}/process"
+                                        class="btn btn-primary btn-sm my-1 mr-sm-1 btn-block"
+                                        onclick="return confirm('Proses surat ini ?')"><i class="nav-icon fas fa-sync"></i>
+                                        Process</a>
+                                        @endif
+                                        @if($data->status == 'diproses')
+                                    <a href="/peralihanwaris/{{$data->id}}/upload-akta"
+                                        class="btn btn-primary btn-sm my-1 mr-sm-1 btn-block"
+                                        onclick="return confirm('Konfirmasi surat ini ?')"><i class="nav-icon fas fa-file-alt"></i>
+                                        Confirm</a>
+                                        @endif
+                                        @if($data->status == 'pending' || $data->status == 'diproses' )
+                                <a href="/peralihanwaris/{{$data->id}}/reject"
+                                    class="btn btn-danger btn-sm my-1 mr-sm-1 btn-block"
+                                    onclick="return confirm('Reject surat ini ?')"><i class="nav-icon fas fa-times"></i>
+                                    Reject</a>
+                                        @endif
+                                {{-- <a href="/peralihanwaris/{{$data->id}}/upload-akta"
                                     class="btn btn-primary btn-sm my-1 mr-sm-1 btn-block"
                                     onclick="return confirm('Konfirmasi surat ini ?')"><i class="nav-icon fas fa-file-alt"></i>
                                     Confirm</a>
@@ -103,7 +122,7 @@
                                 <a href="/peralihanwaris/{{$data->id}}/reject"
                                     class="btn btn-danger btn-sm my-1 mr-sm-1 btn-block"
                                     onclick="return confirm('Reject surat ini ?')"><i class="nav-icon fas fa-times"></i>
-                                    Reject</a>
+                                    Reject</a> --}}
                                 <a href="/peralihanwaris/{{$data->id}}/delete"
                                     class="btn btn-danger btn-sm my-1 mr-sm-1 btn-block"
                                     onclick="return confirm('Hapus Data ?')"><i class="nav-icon fas fa-trash"></i>
